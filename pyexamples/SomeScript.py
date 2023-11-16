022aa50aa922dec6dde4d0ab45149b60269bcefa
wassim <wassim@apc.in2p3.fr>
2023-11-16 20:58:41 +0100

    Some more stuff

--- pyexamples/SomeScript.py
+++ pyexamples/SomeScript.py
@@ -10,7 +10,7 @@ arch = [
     to_begin(),
     
     #input
-    to_input( pathfile='../examples/fcn8s/cats.jpg' ,name="input_image", caption="Input Image"),
+    to_input( pathfile='SEGMENT_INPUT.png' ,name="input_image", caption="Input Image"),
 
     #block-001
     to_ConvConvRelu( name='ccr_b1', s_filer="128x160", n_filer=(128,128), offset="(0,0,0)", to="(0,0,0)", width=(2,2), height=40, depth=40  ),
@@ -26,19 +26,19 @@ arch = [
     #to_connection( "pool_b4", "ccr_b5"),
 
     #Decoder
-    *block_Unconv( name="b6", bottom="pool_b4", top='end_b6', s_filer="16x20",  n_filer=32, offset="(2.1,0,0)", size=(16,16,5.0), opacity=0.5 ),
+    *block_Unconv( name="b6", bottom="pool_b4", top='end_b6', s_filer="16x20",  n_filer=16, offset="(2.1,0,0)", size=(16,16,5.0), opacity=0.5 ),
     to_skip( of='ccr_b4', to='ccr_b6', pos=1.25),
-    *block_Unconv( name="b7", bottom="end_b6", top='end_b7', s_filer="32x40", n_filer=64, offset="(2.1,0,0)", size=(25,25,4.5), opacity=0.5 ),
+    *block_Unconv( name="b7", bottom="end_b6", top='end_b7', s_filer="32x40", n_filer=32, offset="(2.1,0,0)", size=(25,25,4.5), opacity=0.5 ),
     to_skip( of='ccr_b3', to='ccr_b7', pos=1.25),    
-    *block_Unconv( name="b8", bottom="end_b7", top='end_b8', s_filer="64x80", n_filer=32, offset="(2.1,0,0)", size=(32,32,3.5), opacity=0.5 ),
+    *block_Unconv( name="b8", bottom="end_b7", top='end_b8', s_filer="64x80", n_filer=64, offset="(2.1,0,0)", size=(32,32,3.5), opacity=0.5 ),
     to_skip( of='ccr_b2', to='ccr_b8', pos=1.25),    
-    *block_Unconv( name="b9", bottom="end_b8", top='end_b9', s_filer="128x160", n_filer=16,  offset="(2.1,0,0)", size=(40,40,2.5), opacity=0.5 ),
+    *block_Unconv( name="b9", bottom="end_b8", top='end_b9', s_filer="128x160", n_filer=128,  offset="(2.1,0,0)", size=(40,40,2.5), opacity=0.5 ),
     to_skip( of='ccr_b1', to='ccr_b9', pos=1.25),
     
-    to_ConvSoftMax( name="soft1", s_filer="512", offset="(0.75,0,0)", to="(end_b9-east)", width=1, height=40, depth=40, caption="128x160\\\\SIGMOID" ),
+    to_ConvSoftMax( name="soft1", s_filer="128x160", offset="(0.75,0,0)", to="(end_b9-east)", width=1, height=40, depth=40, caption="128x160\\\\SIGMOID" ),
     to_connection( "end_b9", "soft1"),
-    to_input( pathfile='../examples/fcn8s/cats.jpg' ,x = 3, offset="(0,5,0)", to="(soft1-east)",name="output_image", caption="Output Probabilistic map"),
-    to_input( pathfile='../examples/fcn8s/cats.jpg' ,x = 3, offset="(0,-5,0)", to="(soft1-east)",name="label_image", caption="Label (GT)"),
+    to_input( pathfile='SEGMENT_PREDIT.png' ,x = 3, offset="(0,5,0)", to="(soft1-east)",name="output_image", caption="Output Probabilistic map"),
+    to_input( pathfile='SEGMENT_GT.png' ,x = 3, offset="(0,-5,0)", to="(soft1-east)",name="label_image", caption="Label (GT)"),
 
     #Legend
     to_Conv( name="conv_legend", s_filer="", n_filer="", offset="(3,-5,0)",  to="(ccr_b1-south)", width=4, height=2, depth=2, caption="Convolution" ),
